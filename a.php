8e999b74bb9268e072ae9d2234168ea0161c6b0d
zhaobiao <zhaobiao@zhaobiao-PC>
2015-11-05 10:12:26 +0800

    第一次提交

---
+++ a.php
@@ -0,0 +1,16 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+
+$json_arr = array("username"=>1,"age"=>2,"job"=>3);
+$json_obj = json_encode($json_arr);
+echo $json_obj;
+
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>我的主页</title>
+    </head>
+    <script type="text/javascript">
+ 
+    </script>
+    <body>
+        <form id="fm1" method="post" action="">
+            <input name="username" type="text" value="">
+            <input name="sb" type="submit" value="提交">
+        </form>
+        <?php
+            $con=mysql_connect("localhost","root","");
+
+            
+            mysql_select_db("test",$con);
+
+            
+            @$username =$_POST['username'];
+            $sql = "insert into tb_user(name) values ('$username')";
+            mysql_query($sql,$con);
+            
+
+
+        ?>
+    </body>
+</html>
