87f00048eefaeb2e842bff66dab1beed24dcb587
zhaoyiyang <zhaoyiyang@126.com>
2016-08-04 17:21:24 +0800

    略作修改

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
         <title>我的主页</title>
     </head>
     <script type="text/javascript">
